6861ee44a693007ce51d9437258008b1e6fec62e
Francisco Monroy <83427525+fmonroy75@users.noreply.github.com>
2024-11-19 17:27:37 -0500

    templates iniciales

--- proyecto_inmuebles/gestion_inmuebles/urls.py
+++ proyecto_inmuebles/gestion_inmuebles/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
 from . import views
+
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", views.indice , name="indice"),
+    ]
