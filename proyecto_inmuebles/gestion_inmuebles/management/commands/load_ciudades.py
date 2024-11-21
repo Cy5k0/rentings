bec395929f8a7d841c87ffa2ff0feda6b4f892e1
Cy5k0 <francisco.colomer@gmail.com>
2024-11-21 17:25:42 -0300

    fix menores de loaddata ver comentarios linux-win

--- proyecto_inmuebles/gestion_inmuebles/management/commands/load_ciudades.py
+++ proyecto_inmuebles/gestion_inmuebles/management/commands/load_ciudades.py
@@ -1,15 +1,17 @@
-#id, estado_provincia_id , nombre
+# id, estado_provincia_id , nombre
 
 import csv
 from django.core.management.base import BaseCommand
 from gestion_inmuebles.models import Ciudad
 
 
-
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        archivo = open('data/ciudades.csv', 'r')
-        reader = csv.reader(archivo, delimiter=';')
-        next(reader) # Se salta la primera linea
+        # archivo = open('data/ciudades.csv', 'r') para windows
+        archivo = open("../proyecto_inmuebles/data/ciudades.csv", "r")
+        reader = csv.reader(archivo, delimiter=";")
+        next(reader)  # Se salta la primera linea
         for fila in reader:
-                Ciudad.objects.create(id=fila[0],estado_provincia_id=fila[1], nombre=fila[2])
+            Ciudad.objects.create(
+                id=fila[0], estado_provincia_id=fila[1], nombre=fila[2]
+            )
--- proyecto_inmuebles/gestion_inmuebles/management/commands/load_pais.py
+++ proyecto_inmuebles/gestion_inmuebles/management/commands/load_pais.py
@@ -3,11 +3,11 @@ from django.core.management.base import BaseCommand
 from gestion_inmuebles.models import Pais
 
 
-
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        archivo = open('data/pais.csv', 'r')
-        reader = csv.reader(archivo, delimiter=';')
-        next(reader) # Se salta la primera linea
+        archivo = open("/data/paises.csv", "r")
+        # archivo = open("../proyecto_inmuebles/data/paises.csv", "r") Para Linux
+        reader = csv.reader(archivo, delimiter=";")
+        next(reader)  # Se salta la primera linea
         for fila in reader:
-                Pais.objects.create(id=fila[0], nombre=fila[1])
+            Pais.objects.create(id=fila[0], nombre=fila[1])
