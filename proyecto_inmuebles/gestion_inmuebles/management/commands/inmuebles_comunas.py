dc63ada7be5c978232265fb2c0c09922c97ac643
Cy5k0 <francisco.colomer@gmail.com>
2024-11-22 19:35:14 -0300

    fix inmuebles por columna

--- proyecto_inmuebles/gestion_inmuebles/management/commands/inmuebles_comunas.py
+++ proyecto_inmuebles/gestion_inmuebles/management/commands/inmuebles_comunas.py
@@ -7,14 +7,25 @@ class Command(BaseCommand):
     help = "Consulta listado de inmuebles para arriendo separado por comunas"
 
     def handle(self, *args, **options):
-        inmuebles = Inmueble.objects.all().values(
-            "nombre", "descripcion", "ciudad__nombre"
+        inmuebles = (
+            Inmueble.objects.all()
+            .values("nombre", "descripcion", "ciudad__nombre")
+            .distinct()
+            .order_by("ciudad__nombre")
         )
+
         with open("inmuebles_por_comunas.txt", "w", encoding="utf-8") as archivo:
+            comuna_actual = None
             for inmueble in inmuebles:
-                archivo.write(
-                    f"Nombre: {inmueble['nombre']}, Descripción: {inmueble['descripcion']}, Comuna: {inmueble['ciudad__nombre']}\n"
-                )
+                if comuna_actual != inmueble["ciudad__nombre"]:
+                    comuna_actual = inmueble["ciudad__nombre"]
+                    if comuna_actual != inmuebles[0]["ciudad__nombre"]:
+                        archivo.write("\n")
+                    archivo.write(f"COMUNA {comuna_actual.upper()}\n")
+                    archivo.write("═" * 50 + "\n")
+
+                archivo.write(f"► {inmueble['nombre']}\n")
+                archivo.write(f"  {inmueble['descripcion']}\n\n")
         self.stdout.write(
             self.style.SUCCESS(
                 "Listado de inmuebles guardado en inmuebles_por_comunas.txt"
